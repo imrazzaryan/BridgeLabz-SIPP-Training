aee077d42288fb89e7a4cc410589524aeaa5ef55
imrazzaryan <rajaryan30092004@gmail.com>
2025-06-24 21:38:51 +0530

    Files deleted

--- JavaFundamentals/KmToMiles.java
+++
@@ -1,8 +0,0 @@
-public class KmToMiles {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers * 0.625;  // 1 km = 0.625 miles
-
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
